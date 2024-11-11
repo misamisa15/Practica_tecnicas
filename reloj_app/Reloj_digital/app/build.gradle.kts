6cddc4dcee8d02da250a9eb934b354ad5b632523
ari <aaspiazu8623@utm.edu.ec>
2024-11-10 22:31:46 -0500

    Proyecto reloj completo en android studio

---
+++ reloj_app/Reloj_digital/app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+}
+
+android {
+    namespace = "com.example.reloj_digital"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.reloj_digital"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ reloj_app/Reloj_digital/app/src/main/java/com/example/reloj_digital/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.reloj_digital;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txtH;
+    TextView txtM;
+    TextView txtS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtH = findViewById(R.id.hora);
+        txtM = findViewById(R.id.minuto);
+        txtS = findViewById(R.id.segundo);
+
+        relojInicio();
+    }
+
+    private void relojInicio() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int hora = 0;
+                int minuto = 0;
+                int segundo = 0;
+
+                while (hora < 24) {
+                    while (minuto < 60) {
+                        while (segundo < 60) {
+                            int finalHora = hora;
+                            int finalMinuto = minuto;
+                            int finalSegundo = segundo;
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    txtH.setText(String.format("%02d", finalHora));
+                                    txtM.setText(String.format("%02d", finalMinuto));
+                                    txtS.setText(String.format("%02d", finalSegundo));
+                                }
+                            });
+
+                            segundo++;
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        segundo = 0;
+                        minuto++;
+                    }
+                    minuto = 0;
+                    hora++;
+                }
+            }
+        });
+        thread.start();
+    }
+}
